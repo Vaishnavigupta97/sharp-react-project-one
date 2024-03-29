b3f7748e68f89029870d01ac602ee47d58c5b842
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2024-03-29 17:30:18 +0530

    feature working

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ExpenseItem from "./components/ExpenseItem";
+
+function App() {
+  return (
+    <div className="App">
+    <h1>I am trying to learn react</h1>
+    <ExpenseItem />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem() {
+    return (
+        <>
+            <div>15 August 2023</div>
+            <div>Delhi</div>
+            <div>
+                <h2>Book</h2>
+                <div>$10</div>
+            </div>
+        </>
+    )
+}
+export default ExpenseItem;
