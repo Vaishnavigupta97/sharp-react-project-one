9922da78a281cb036e641ddc5514d4773e3f80c6
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2024-03-30 13:17:36 +0530

    add delete Expense Button

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,18 @@
 // Write your code here
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
+import { useState } from "react";
 
 function ExpenseItem(props){  
+  // let price = props.price;
+  let [price, setPrice] = useState(props.price);
   return (
     <div className="expense-item">
       <ExpenseDate date = {props.date} />
       <div className="expense-item__description">
         <h2>{props.title}</h2>
-        <div className="expense-item__price">${props.price}</div>
+        <div className="expense-item__price">${price}</div>
+        <button onClick={() => setPrice(0)}>Delete Expense</button>
       </div>
     </div>
   );
