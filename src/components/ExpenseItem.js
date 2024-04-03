e33658ed4bd9384cc90a7bd25f1c7a7e1c0307f1
Vaishnavigupta97 <vaishnavigupta6389@gmail.com>
2024-04-03 11:25:35 +0530

    add title with help of useState hook

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -5,15 +5,16 @@ import { useState } from "react";
 
 function ExpenseItem(props){  
   // let price = props.price;
-  const changeTitle = () => {
-    console.log("change Title");
-  }
   let [price, setPrice] = useState(props.price);
+  const [title, setTitle] = useState(props.title);
+  const changeTitle = () => {
+    setTitle("new Title");
+  }
   return (
     <div className="expense-item">
       <ExpenseDate date = {props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">${price}</div>
         <button onClick={() => setPrice(0)}>Delete Expense</button>
       </div>
